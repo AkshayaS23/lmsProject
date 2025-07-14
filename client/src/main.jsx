3e8bbf9fa72ee9f43b9bd8f94b58f4042cd25ffd
Akshaya <akshayaviswanathan8@gmail.com>
2025-07-14 13:28:32 +0530

    updated

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -13,7 +13,7 @@ if (!PUBLISHABLE_KEY) {
 
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/lmsProject">
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
       <AppContextProvider>
         <App />
