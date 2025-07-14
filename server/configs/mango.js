9e2a96d3a5ce0d9cccf4618ca93f75a9bc9e05bd
Akshaya <akshayaviswanathan8@gmail.com>
2025-07-14 12:53:00 +0530

    updated

--- server/configs/mango.js
+++ server/configs/mango.js
@@ -1,11 +1,11 @@
-import mangoose from "mongoose";
+import mongoose from "mongoose";
 
 // connect to the MangoDb database
 
 const connect = async ()=> {
-    mangoose.connection.on('connected', ()=> console.log('Database Connected'))
+    mongoose.connection.on('connected', ()=> console.log('Database Connected'))
 
-    await mangoose.connect(`${process.env.MANGODB_URI}/lms`)
+    await mongoose.connect(`${process.env.MANGODB_URI}/lms`)
 }
 
 export default connect
